4b2ecd502ecb378607f057fb8b225577a5e6eaa2
Luis Miguel Baez Aponte <lmbaeza@unal.edu.co>
2020-12-10 20:02:24 -0500

    RSA was updated

--- rsa.py
+++ rsa.py
@@ -14,6 +14,7 @@ def fastpow(x, y, p):
         x = (x * x) % p
     return res
 
+
 class RSA:
     def __init__(self, bits=112, DEBUG=False):
         self.p = Crypto.Util.number.getPrime(bits, randfunc=Crypto.Random.get_random_bytes)
@@ -22,6 +23,8 @@ class RSA:
         self.phi = (self.p-1)*(self.q-1)
         self.e = 65537
         self.d = Crypto.Util.number.inverse(self.e, self.phi)
+        self.bits = bits
+        self.DEBUG = DEBUG
 
         if DEBUG:
             print("p:   %d\nq:   %d\nn:   %d\nphi: %d\ne:   %d\nd:   %d\nbit: %d\n" % (
@@ -35,9 +38,11 @@ class RSA:
             ascii = ord(message[i]) # Ascii code of message[i]
             cipher[i] = fastpow(ascii, self.e, self.n)
             # ci = (mi**e) % n
-        return cipher
+        # print(cipher)
+        return self.list_to_hex(cipher)
     
-    def decrypt(self, cipher) :
+    def decript(self, cipher) :
+        cipher = self.hex_to_list(cipher)
         sz = len(cipher)
         message = ""
         for i in range(sz):
@@ -45,6 +50,21 @@ class RSA:
             # mi = (ci**d) % n
             message += chr(ascii)
         return message
+
+    def list_to_hex(self, cipher):
+        cipher_hex = ""
+        for value in cipher:
+            number = hex(value)[2:]
+            diff = self.bits//2 - len(number)
+            number = "0"*diff + number
+            cipher_hex += number
+        return cipher_hex
+
+    def hex_to_list(self, cipher_hex):
+        cipher_list = []
+        for i in range(0, len(cipher_hex), self.bits//2):
+            cipher_list.append(int(cipher_hex[i: i+self.bits//2], 16))
+        return cipher_list
     
 
 if __name__ == '__main__':
@@ -52,6 +72,5 @@ if __name__ == '__main__':
     text = "Message to encrypt with RSA"
     cipher = rsa.encrypt(text)
     print("Cipher:", cipher, "\n\n")
-    decrypted_message = rsa.decrypt(cipher)
-    print("Decrypted Message:", decrypted_message)
-
+    decripted_message = rsa.decript(cipher)
+    print("Decripted Message:", decripted_message)
